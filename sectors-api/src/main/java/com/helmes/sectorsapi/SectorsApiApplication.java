aacfca34cdbaa5d767c7bf4886ecd18149cebe90
edgsel <edgar.selihov@gmail.com>
2026-01-25 16:45:37 +0200

    HLMS-000 - formating

--- sectors-api/src/main/java/com/helmes/sectorsapi/SectorsApiApplication.java
+++ sectors-api/src/main/java/com/helmes/sectorsapi/SectorsApiApplication.java
@@ -1,4 +1,4 @@
-package com.helmes.sectorsApi;
+package com.helmes.sectorsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SectorsApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SectorsApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SectorsApiApplication.class, args);
+    }
 }
