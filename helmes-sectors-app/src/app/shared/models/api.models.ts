2b1ff83db3ad9915556ea699daa55577907b6ac7
edgsel <edgar.selihov@gmail.com>
2026-01-30 17:19:11 +0200

    HLMS-003 - add api models

---
+++ helmes-sectors-app/src/app/shared/models/api.models.ts
@@ -0,0 +1,50 @@
+export interface UserAuthDTO {
+  username: string;
+  password: string;
+}
+
+export interface AuthResponseDTO {
+  jwt: string;
+}
+
+export interface CreateApplicationDTO {
+  applicantName: string;
+  sectorIds: number[];
+  agreedToTerms: boolean;
+}
+
+export interface UpdateApplicationDTO {
+  applicantName: string;
+  sectorIds: number[];
+}
+
+export interface ApplicationResponseDTO {
+  applicationId: string;
+  applicantName: string;
+  sectorIds: number[];
+  agreedToTerms: boolean;
+}
+
+export interface ApplicationSummaryResponseDTO {
+  id: string;
+  createdAt: string;
+  updatedAt: string | null;
+}
+
+export interface SectorResponseDTO {
+  id: number;
+  name: string;
+  parentId: number | null;
+  sectorLevel: number;
+  children: SectorResponseDTO[];
+}
+
+export interface ErrorResponseDTO {
+  code: string;
+  description: string;
+}
+
+export interface Notification {
+  message: string;
+  type: 'success' | 'error';
+}
