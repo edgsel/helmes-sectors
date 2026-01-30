cbd1ebf6472c847e545debc3eaa16a40459095aa
edgsel <edgar.selihov@gmail.com>
2026-01-30 14:27:08 +0200

    HLMS-002 - formatting

--- sectors-api/src/main/java/com/helmes/sectorsapi/exception/EntityNotFoundException.java
+++ sectors-api/src/main/java/com/helmes/sectorsapi/exception/EntityNotFoundException.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class EntityNotFoundException extends RuntimeException {
+
     private final String code;
 
     public EntityNotFoundException(String description, String code) {
