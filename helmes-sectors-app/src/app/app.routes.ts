482cb18aa925ccccdc7fe12950b2c3b434238399
edgsel <edgar.selihov@gmail.com>
2026-01-31 13:18:32 +0200

    HLMS-003 - add new page routes

--- helmes-sectors-app/src/app/app.routes.ts
+++ helmes-sectors-app/src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/applications', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login.component')
+      .then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./features/auth/register/register.component')
+      .then(m => m.RegisterComponent)
+  },
+  {
+    path: 'applications',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./features/applications/application-list/application-list.component')
+          .then(m => m.ApplicationListComponent)
+      },
+      {
+        path: 'new',
+        loadComponent: () => import('./features/applications/application-create/application-create.component')
+          .then(m => m.ApplicationCreateComponent)
+      },
+      {
+        path: ':id/edit',
+        loadComponent: () => import('./features/applications/application-edit/application-edit.component')
+          .then(m => m.ApplicationEditComponent)
+      }
+    ]
+  },
+  { path: '**', redirectTo: '/applications' }
+];
